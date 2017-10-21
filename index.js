01a647651bc40e6c02de01e59b75de823ee1899e
Mike <zluo@alumni.berklee.edu>
2017-10-21 18:12:40 -0400

    solved inOrder

---
+++ index.js
@@ -0,0 +1,10 @@
+function inOrder(currentNode) {
+  if (currentNode.left) {
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data);
+
+  if (currentNode.right) {
+    inOrder(currentNode.right)
+  }
+}
